691ca11a4f03b757ecdb4f47c74e044c379e8e14
Joshua Dull <Joshualdull@gmail.com>
2020-02-27 16:24:56 -0500

    debugging isotope

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,44 +1,16 @@
 // init Isotope
 var $grid = $('.grid').isotope({
-    itemSelector : '.element-item',
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
 });
 
-var filters = {} ;
-
-// Button click actions
-$('#schoolfilter').on('click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter') ;
-    filterValue = filterValue ;
-    $grid.isotope({ filter: filterValue });
-}) ;
-
-
-$('.filters').on( 'click', '.button', function() {
-  var $this = $(this);
-  // get group key
-  var $buttonGroup = $this.parents('.button-group');
-  var filterGroup = $buttonGroup.attr('data-filter-group');
-  // set filter for group
-  filters[ filterGroup ] = $this.attr('data-filter');
-  // combine filters
-  var filterValue = concatValues( filters );
-  // set filter for Isotope
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
 });
-
-// $('#catfilter').on( 'click', 'button', function() {
-//   var filterValue = $( this ).attr('data-filter') ;
-//     filterValue =  filterValue ;
-//     $grid.isotope({ filter: filterValue }) ;
-// });
-
-
-// // bind sort button click
-// $('#namesorts').on( 'click', 'button', function() {
-//     var sortByValue = $(this).attr('data-sort-by');
-//     $grid.isotope({ sortBy: sortByValue });
-// });
-
 // change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
